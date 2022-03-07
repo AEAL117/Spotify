265c2e2d19f3f3afdbad50eb5b8cc9f569a79016
ALFREDO EZEQUIEL ARENAS LEDESMA <ezequiel_arenas@live.com.mx>
2022-03-06 22:44:40 -0600

    Debug code url

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import Dashboard from "../../spotify/Dashboard"
 const code = new URLSearchParams(window.location.search).get("code")
 
 function Home() {
-    
+    console.log("CODIGO DE URL"+code);
     return code!==null ? <Dashboard code={code} /> : <SLogin />
 }
 export default Home;
